4cc475ec043cf2cd583b6348f8f7914ebe12bd1d
rama <rama@rama.rama>
2025-04-30 17:40:20 +0200

    <Remove an unnecessary comment>

--- solib.cpp
+++ solib.cpp
@@ -3,8 +3,7 @@
 #include "Networking/So-networking.hpp"
 int main() {
     So::TestServer server(AF_INET, SOCK_STREAM, 0, 8080, INADDR_ANY, 10); 
-    So::TestServer server1(AF_INET, SOCK_STREAM, 0, 8080, INADDR_ANY, 10);// Custom values// Create a TestServer instance
-    server.launch();  
-    server1.launch();     // Launch the server
+    So::TestServer server1(AF_INET, SOCK_STREAM, 0, 8080, INADDR_ANY, 10);
+    server1.launch();
     return 0;
 }
